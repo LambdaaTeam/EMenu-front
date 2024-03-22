f1b26ed71d93d1f6cf27e63fb517bc06a2270b9d
Psykka <36780964+Psykka@users.noreply.github.com>
2024-03-22 19:34:09 -0300

    fix: login screen

--- menu/src/components/Login.jsx
+++ menu/src/components/Login.jsx
@@ -1,41 +1,40 @@
 import './Login.css';
-import { TextInputMask } from 'react-native-masked-text';
+
 import { useState } from 'react';
 
 const Login = () => {
-    const [cell, setCell] = useState('');
+  const [cell, setCell] = useState('');
 
   return (
     <div className='container'>
-        <div className='box-login'>
+      <div className='box-login'>
 
-        
+
         <div className='wrap-welcome'>
-           <span className='bem-vindo'> <p>  Bem-vindo, </p> Que bom ver você! </span> 
+          <span className='bem-vindo'> <p>  Bem-vindo, </p> Que bom ver você! </span>
         </div>
-        
+
         <div className='wrap-login'>
-            <form className="login-form"> 
-            <div className='input-name'> 
-            <span className='span-dados'> Nome Completo </span>
-            <input className='name' type="text" placeholder='Insira seu nome' required="true"/>
+          <form className="login-form">
+            <div className='input-name'>
+              <span className='span-dados'> Nome Completo </span>
+              <input className='name' type="text" placeholder='Insira seu nome' required="true" />
             </div>
 
             <div className='input-cpf'>
-            <span className='span-dados'> CPF </span>
-            <TextInputMask style={StyleSheet.input} type={'cpf'} value={cpf} onChangeText={text => setCpf(text)}/>
-            <input className='cpf' type="number" placeholder='000.000.000-00' required="true" />
+              <span className='span-dados'> CPF </span>
+              <input className='cpf' type="number" placeholder='000.000.000-00' required="true" />
             </div>
 
-            <div className='lembrar-me'> 
-            <input className='remember-me' type="checkbox" /> <span className='remember'> Lembre de mim </span>
-            <div className='wrap-login-btn'> </div>
-            <button className='login-btn'> Entrar </button>
+            <div className='lembrar-me'>
+              <input className='remember-me' type="checkbox" /> <span className='remember'> Lembre de mim </span>
+              <div className='wrap-login-btn'> </div>
+              <button className='login-btn'> Entrar </button>
             </div>
-            </form>
-            </div>
+          </form>
         </div>
-            
+      </div>
+
     </div>
   )
 }
