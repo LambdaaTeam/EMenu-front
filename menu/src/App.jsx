b7d40b1ccd7d9a34a7908ebc243ab8f5985aebc6
ikiyup <amandandrade.01@gmail.com>
2024-04-03 16:56:00 -0300

    tabs and cards

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -1,5 +1,10 @@
-import Login from './components/Login'
+import Login from './components/Login/Login'
+import Header from './components/Header/Headerr'
+import Menu from './components/Header/Menu'
+
+import Home from './components/Home/Home'
 import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   
@@ -7,7 +12,8 @@ function App() {
   return (
     <>
       <div>
-        <Login/>
+        <Header/>
+        <Menu/>
       </div>
       
     </>
---
+++ menu/src/data/pizza.js
@@ -0,0 +1,8 @@
+export const PizzasList = {
+    Nome: "Pizza 4 queijos",
+    Tamanho: "Grade",
+    Ingredientes: "Muçarela, Orégano, Parmesão, Provolone, Salsa e Tomate",
+    Borda: "Catupiry",
+    Price: "R$50,0",
+    Observação: ""
+}
