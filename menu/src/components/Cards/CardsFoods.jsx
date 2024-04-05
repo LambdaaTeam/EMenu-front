8ca5370f20906f3f02d0be2a3473782f19958e13
ikiyup <amandandrade.01@gmail.com>
2024-04-05 17:20:27 -0300

    ShowDetails dos Cards ad

--- menu/src/components/Cards/CardsFoods.jsx
+++ menu/src/components/Cards/CardsFoods.jsx
@@ -8,25 +8,21 @@ import {
 const CardsFoods = ({ cardsFood }) => {
     const { name, price, image, description } = cardsFood;
     return (
-        <>
+        <Card h="42"
+            direction={{ base: 'column', sm: 'row' }}
+            variant="unstyled"
+        >
+            <Image
+                objectFit='cover'
+                width="80px" height="81px"
+                src={image}
+            />
 
-            <Card width="100%" height="79px"
-                direction={{ base: 'column', sm: 'row' }}
-                variant="unstyled"
-
-
-            >
-                <Image
-                    objectFit='cover'
-                    width="80px" height="81px"
-                    src={image}
-                />
-
-                <Stack bg="#FFF" width="297px" height="79px" padding="15px" textAlign="left" borderRadius="0px 24px 24px 0px" display="flex">
-                    <CardBody  >
-                        <Heading fontSize="14px"> {name} </Heading>
-                        <Stack display="flex">
-                        <Icon viewBox='0 0 200 200' boxSize="4px"color='red.500' >
+            <Stack bg="#FFF" textAlign="left" display="flex">
+                <CardBody  >
+                    <Heading fontSize="14px"> {name} </Heading>
+                    <Stack display="flex">
+                        <Icon viewBox='0 0 200 200' boxSize="4px" color='red.500' >
                             <path
                                 fill='currentColor'
                                 d='M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'
@@ -36,13 +32,13 @@ const CardsFoods = ({ cardsFood }) => {
                             {description}
                         </Text>
                         <Text textAlign="right" bottom="25px" color="#000" fontWeight="bold" fontFamily="12px"> R$ {price} </Text>
-                        </Stack>
-                    </CardBody>
-                </Stack>
-            </Card>
+                    </Stack>
+                </CardBody>
+            </Stack>
+        </Card>
 
 
-        </>
+
     )
 }
 
